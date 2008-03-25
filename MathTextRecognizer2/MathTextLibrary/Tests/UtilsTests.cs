687082496a2ff83445968522f83465da774fb00a
luisromangz <luisromangz@469999be-a832-0410-be73-47293d54b917>
2008-03-25 18:18:36 +0000

    · Unit tests for utils added.

---
+++ MathTextRecognizer2/MathTextLibrary/Tests/UtilsTests.cs
@@ -0,0 +1,21 @@
+// UtilsTests.cs created with MonoDevelop
+// User: luis at 19:18 25/03/2008
+//
+
+using System;
+using NUnit.Framework;
+
+namespace MathTextLibrary
+{
+	
+	
+	[TestFixture()]
+	public class UtilsTests
+	{
+		
+		[Test()]
+		public void TestCase()
+		{
+		}
+	}
+}
