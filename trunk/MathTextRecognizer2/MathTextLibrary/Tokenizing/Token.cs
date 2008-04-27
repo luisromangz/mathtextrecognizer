27ebde5bcfdb38bbb1b8c507b318a4578f33ab3c
luisromangz <luisromangz@469999be-a832-0410-be73-47293d54b917>
2008-04-27 12:16:19 +0000

    · DatabasePropertiesEditorDialog has its focus on the description entry. · Added tokenizing folders, widget, and other infrastructure to MathTextRecognizer.

---
+++ trunk/MathTextRecognizer2/MathTextLibrary/Tokenizing/Token.cs
@@ -0,0 +1,22 @@
+// Token.cs created with MonoDevelop
+// User: luis at 13:01 27/04/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+
+namespace MathTextLibrary.Tokenizing
+{
+	
+	/// <summary>
+	/// This class represents a product of lexical analisys.
+	/// </summary>
+	public class Token
+	{
+		
+		public Token()
+		{
+		}
+	}
+}
---
+++ trunk/MathTextRecognizer2/MathTextLibrary/Tokenizing/Tokenizer.cs
@@ -0,0 +1,22 @@
+// Tokenizer.cs created with MonoDevelop
+// User: luis at 13:01 27/04/2008
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+
+namespace MathTextLibrary
+{
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Tokenizer
+	{
+		
+		public Tokenizer()
+		{
+		}
+	}
+}
