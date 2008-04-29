aaddcf7466a71bb1ebdc8d25d744a3b025352668
luisromangz <luisromangz@469999be-a832-0410-be73-47293d54b917>
2008-04-29 11:42:55 +0000

    · LexicalRulesManagerDialog completed but for the actual rules managment. · Added LexicalRuleEditorDialog.

---
+++ trunk/MathTextRecognizer2/MathTextRecognizer/Stages/Dialogs/LexicalRuleEditorDialog.cs
@@ -0,0 +1,38 @@
+// LexicalRuleEditorDialog.cs created with MonoDevelop
+// User: luis at 13:06 29/04/2008
+
+using System;
+using Gtk;
+using Glade;
+
+using MathTextLibrary.Analisys.Lexical;
+
+namespace MathTextRecognizer
+{
+	
+	/// <summary>
+	/// Implements a dialog which purpose is to edit and create 
+	/// instances of the <c>LexicalRule</c> class.
+	/// </summary>
+	public class LexicalRuleEditorDialog
+	{
+		
+		/// <summary>
+		/// <c>LexicalRuleEditorDialog</c>'s constructor method.
+		/// </summary>
+		/// <param name="parent">
+		/// The window this dialog will be modal to.
+		/// </param>
+		public LexicalRuleEditorDialog(Window parent)
+		{
+		}
+		
+		
+		class LexicalExpressionWidget : Alignment
+		{
+			
+		}
+	}
+	
+	
+}
--- trunk/MathTextRecognizer2/MathTextRecognizer/Stages/Dialogs/LexicalRulesManagerDialog.cs
+++ trunk/MathTextRecognizer2/MathTextRecognizer/Stages/Dialogs/LexicalRulesManagerDialog.cs
@@ -10,6 +10,9 @@ using System.Collections.Generic;
 using Gtk;
 using Glade;
 
+using MathTextCustomWidgets.Dialogs;
+using MathTextLibrary.Analisys.Lexical;
+
 namespace MathTextRecognizer
 {
 	
@@ -34,6 +37,9 @@ namespace MathTextRecognizer
 		
 		[WidgetAttribute]
 		private Button editBtn = null;
+		
+		[WidgetAttribute]
+		private Button removeBtn = null;
 		
 #endregion Glade widgets
 		
@@ -60,7 +66,38 @@ namespace MathTextRecognizer
 			
 			lexicalRulesManagerDialog.TransientFor = parent;
 			
+			InitializeWidgets();
+			
 		}
+		
+#region Properties
+		/// <value>
+		/// Contains the rules used in the lexical analisys.
+		/// </value>
+		public List<LexicalRule> LexicalRules
+		{			
+			get
+			{
+				// We extract the rules from the treeview's model.
+				List<LexicalRule> rules = new List<LexicalRule>();
+				foreach (object[] values in rulesStore) 
+				{
+					rules.Add((LexicalRule)(values[2]));
+				}
+				
+				return rules;
+			}
+			
+			set
+			{
+				// Inserts the rules in the model.
+				foreach (LexicalRule rule in value) 
+				{
+					AddRule(rule);
+				}
+			}
+		}
+#endregion Properties
 		
 #region Public methods
 		
@@ -88,11 +125,45 @@ namespace MathTextRecognizer
 #region Non-public methods
 		
 		/// <summary>
+		/// Adds a new rule to the list.
+		/// </summary>
+		/// <param name="rule">
+		/// The rule which will be added.
+		/// </param>
+		private void AddRule(LexicalRule rule)
+		{
+			string expression =  
+				String.Join(" | ", rule.LexicalExpressions.ToArray());
+			
+			TreeIter newIter = 	rulesStore.AppendValues(rule.RuleName,
+			                                            expression,
+			                                            rule);
+			
+			// We select the new row and scroll to it.
+			rulesTV.Selection.SelectIter(newIter);
+			rulesTV.ScrollToCell(rulesTV.Selection.GetSelectedRows()[0],
+			                     rulesTV.Columns[0],
+			                     true, 1.0f, 0);
+		}
+		
+		/// <summary>
 		/// Initializes the dialog's widgets;
 		/// </summary>
 		private void InitializeWidgets()
 		{
+			// We create a model for the tree view.
+			rulesStore = new ListStore(typeof(string),
+			                           typeof(string),
+			                           typeof(LexicalRule));
 			
+			rulesTV.Model = rulesStore;
+			
+			// We add the columns to the view.
+			rulesTV.AppendColumn("Regla", new CellRendererText(),"text", 0);
+			rulesTV.AppendColumn("Expresión", new CellRendererText(),"text", 1);			
+			
+			rulesTV.Selection.Changed += 
+				new EventHandler(OnRulesTVSelectionChanged);
 		}
 		
 		/// <summary>
@@ -108,6 +179,14 @@ namespace MathTextRecognizer
 		public void OnAddBtnClicked(object sender, EventArgs args)
 		{
 			
+			// TODO Add an actual LexicalRule.
+			LexicalRule rule = new LexicalRule();
+			rule.RuleName = "MEH";
+			rule.LexicalExpressions.Add("[0-9]+");
+			rule.LexicalExpressions.Add(@"[0-9]+\.[0-9]+");
+			
+			AddRule(rule);
+			
 		}
 
 		/// <summary>
@@ -122,6 +201,21 @@ namespace MathTextRecognizer
 		/// </param>
 		public void OnRemoveBtnClicked(object sender, EventArgs args)
 		{			
+			TreeIter selected;
+			rulesTV.Selection.GetSelected(out selected);
+			
+			// We retrieve the selected row's rule name.
+			string ruleName = (string)(rulesStore.GetValue(selected,0));
+			
+			ResponseType res;
+			res = ConfirmDialog.Show(lexicalRulesManagerDialog,
+			                         "¿Realmente quieres eliminar la regla «{0}»?",
+			                         ruleName);
+			
+			if(res == ResponseType.Yes)
+			{
+				rulesStore.Remove(ref selected);
+			}
 		}	
 		
 		/// <summary>
@@ -135,7 +229,20 @@ namespace MathTextRecognizer
 		/// </param>
 		public void OnUpBtnClicked(object sender, EventArgs args)
 		{
+			TreePath selectedPath =  rulesTV.Selection.GetSelectedRows()[0];
+			TreeIter selected;
+			rulesTV.Selection.GetSelected(out selected);
 			
+			TreePath previousPath = rulesTV.Selection.GetSelectedRows()[0];
+			previousPath.Prev();
+			TreeIter previous;
+			rulesStore.GetIter(out previous, previousPath);
+			
+			rulesStore.MoveBefore(selected, previous);
+			
+			rulesTV.Selection.UnselectAll();
+			rulesTV.Selection.SelectPath(previousPath);
+			rulesTV.ScrollToCell(previousPath, rulesTV.Columns[0], true, 0, 0);
 		}
 		
 		/// <summary>
@@ -149,7 +256,19 @@ namespace MathTextRecognizer
 		/// </param>
 		public void OnDownBtnClicked(object sender, EventArgs args)
 		{
+			TreePath selectedPath =  rulesTV.Selection.GetSelectedRows()[0];
+			TreeIter selected;
+			rulesTV.Selection.GetSelected(out selected);
 			
+			TreePath nextPath = rulesTV.Selection.GetSelectedRows()[0];
+			nextPath.Next();
+			TreeIter next;
+			rulesStore.GetIter(out next, nextPath);
+			
+			rulesStore.MoveBefore(selected, next);
+			rulesTV.Selection.UnselectAll();
+			rulesTV.Selection.SelectPath(nextPath);
+			rulesTV.ScrollToCell(nextPath, rulesTV.Columns[0], true, 1.0f, 0);
 		}
 		
 		/// <summary>
@@ -163,7 +282,7 @@ namespace MathTextRecognizer
 		/// </param>
 		public void OnEditBtnClicked(object sender, EventArgs args)
 		{
-			
+			// TODO Edit rule;
 		}
 		
 		/// <summary>
@@ -178,6 +297,34 @@ namespace MathTextRecognizer
 		public void OnSaveBtnClicked(object sender, EventArgs args)
 		{
 			
+		}
+		
+		/// <summary>
+		/// Activates or deactivates the buttons based on the selected rows.
+		/// </summary>
+		/// <param name="sender">
+		/// A <see cref="System.Object"/>
+		/// </param>
+		/// <param name="args">
+		/// A <see cref="EventArgs"/>
+		/// </param>
+		public void OnRulesTVSelectionChanged(object sender, EventArgs args)
+		{
+			bool rowSelected = rulesTV.Selection.CountSelectedRows() > 0;
+			
+			int idx = -1;
+			if(rowSelected)
+			{
+				idx = rulesTV.Selection.GetSelectedRows()[0].Indices[0];
+			}
+			
+	
+			upBtn.Sensitive = rowSelected && idx > 0;
+			downBtn.Sensitive = 
+				rowSelected && idx < rulesStore.IterNChildren() - 1;
+			
+			editBtn.Sensitive = rowSelected;
+			removeBtn.Sensitive = rowSelected;
 		}
 		
 #endregion Non-public methods
